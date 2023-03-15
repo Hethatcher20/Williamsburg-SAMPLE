96c4f481f71a7a2c214673d1d43379a09af352e0
Hanah Thatcher <103278600+Hethatcher20@users.noreply.github.com>
2023-03-15 17:13:08 -0400

    Update data.js

--- History Tour/data.js
+++ History Tour/data.js
@@ -167,7 +167,7 @@ var APP_DATA = {
           "yaw": 0.7455540433138736,
           "pitch": 0.1326374325261206,
           "title": "The Big Spring",
-          "text": "Embed an image"
+          "img": /img/close.png
         }
       ]
     }
